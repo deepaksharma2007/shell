b7701a323ffc3c962f649f39186fbf1b09be0f7d
Deepak Sharma <64480480+deepaksharma2007@users.noreply.github.com>
2024-07-10 09:25:56 +0530

    Create fun.sh

---
+++ fun.sh
@@ -0,0 +1,39 @@
+mylw()
+{
+        echo "Hello Deepak"
+        echo ""
+}
+mylw
+
+
+# Functions with arguments 
+deep()
+{
+        name=${1}
+        echo "my name is ${name}"
+}
+deep raj
+
+
+# Static function 
+myuser()
+{
+        echo " Total user that has bash shell are:- "
+        cat /etc/passwd | grep bash$ | cut -d: -f 1
+}
+myuser
+
+
+
+
+
+#  function with argument
+
+users()
+{
+         echo " Total user that has bash shell are:- "
+        cat /etc/passwd | grep ${1}$ | cut -d: -f 1
+}
+users bash
+
+#users nologin
